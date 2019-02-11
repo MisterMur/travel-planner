67e15aad1b325578fd33841bcfe4b353ae43d263
github username <github email address>
2019-02-11 16:41:36 -0500

    modeled out user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :delete]
+  def new
+  end
+
+  def create
+  end
+
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
 end
