d617c2765a7fff5b821f7792cad080b1d56443f3
github username <github email address>
2019-02-12 17:09:48 -0500

    started auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :authorized
+  helper_method :current_user
 
-  def index
-    render :index
+  def current_user
+    User.find_by({ id: session[:user_id] })
   end
 
-end
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    redirect_to login_path unless logged_in?
+  end
+
+end #end of ApplicationController
